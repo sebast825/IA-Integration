d805a131d2d461869a95421a48e81d1f584eef9f
sebast825 <sebamolina825@gmail.com>
2025-11-01 12:22:59 -0300

    added cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,13 +1,19 @@
 const express = require('express');
-const healthRoutes = require('./routes/healthRoutes');
+import cors from 'cors';
 
+const healthRoutes = require('./routes/healthRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const promptRoutes = require('./routes/promptRoutes');
 
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173', // tu puerto de Vite
+  credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 
+}));
 app.use('/api', healthRoutes);
 app.use('/api', chatRoutes);
 app.use('/api', promptRoutes);
@@ -18,3 +24,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
