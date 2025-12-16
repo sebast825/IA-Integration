a82b435c9d1fb290febdfa22301e390659d55a34
sebast825 <sebamolina825@gmail.com>
2025-12-16 15:05:48 -0300

    replace smiling face with gemini

--- back/src/services/promtService.ts
+++ back/src/services/promtService.ts
@@ -1,27 +1,29 @@
-import OpenAI from "openai";
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
 import "dotenv/config"; 
 
 class PromptService {
-     private readonly client: OpenAI;
-     constructor() {
-       this.client = new OpenAI({
-         baseURL: process.env.BASE_URL_AI,
-         apiKey: process.env.HF_TOKEN,
-       });
-     }
-   
-     async sendPrompt(promt : string): Promise<any> {
-      const plainPrompt = `${promt}\n\nPor favor, responde solo en texto plano. No uses negritas, encabezados, tablas ni listas Markdown.`;
+  private readonly ai;
 
-       const completion = await this.client.chat.completions.create({
-         model: "openai/gpt-oss-120b:cerebras",
-         messages: [
-           { role: "user", content: plainPrompt },
-         ],
-       });
-       var rsta = completion.choices[0].message.content?.toString() || "nope";
-       return rsta;
-     }
+  constructor() {
+    this.ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY,
+
+    });
+  }
+
+  async sendPrompt(promt: string): Promise<any> {
+    const systemInstruction = `nPor favor, responde solo en texto plano. No uses negritas, encabezados, tablas ni listas Markdown.`;
+
+    const response = await this.ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      config: {
+        systemInstruction: systemInstruction,
+      },
+      contents: promt,
+    });
+    return response.text;
+  }
 }
 
 export const promptService = new PromptService();
